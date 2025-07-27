a87def45ac115053578a54486d3f86e980260223
Vanessa941 <nino.parceiro@gmail.com>
2025-07-26 22:41:21 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBjJ1p3lDnFQnDfb9c4hwsI1om4zDL0rHk",
+  authDomain: "comprar-app.firebaseapp.com",
+  projectId: "comprar-app",
+  storageBucket: "comprar-app.firebasestorage.app",
+  messagingSenderId: "840293288941",
+  appId: "1:840293288941:web:fbb3da2a9fa8b8f8ee35e4"
+};
