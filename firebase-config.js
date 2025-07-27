a32f1431b5797ee1b60a906fc1d0e882a1630239
Vanessa941 <nino.parceiro@gmail.com>
2025-07-27 16:25:40 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,19 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBjJ1p3lDnFQnDfb9c4hwsI1om4zDL0rHk",
-  authDomain: "comprar-app.firebaseapp.com",
-  projectId: "comprar-app",
-  storageBucket: "comprar-app.firebasestorage.app",
-  messagingSenderId: "840293288941",
-  appId: "1:840293288941:web:fbb3da2a9fa8b8f8ee35e4"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBjJ1p3lDnFQnDfb9c4hwsI1om4zDL0rHk",
+    authDomain: "comprar-app.firebaseapp.com",
+    projectId: "comprar-app",
+    storageBucket: "comprar-app.firebasestorage.app",
+    messagingSenderId: "840293288941",
+    appId: "1:840293288941:web:fbb3da2a9fa8b8f8ee35e4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
